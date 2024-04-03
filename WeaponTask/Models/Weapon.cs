a934a775e9fa113092364cab6585ed9a6e5fdb73
KaMiL374346363 <kamil.qehreman002@gmail.com>
2024-04-03 04:18:25 +0400

    Weapon task

---
+++ WeaponTask/Models/Weapon.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponTask.Models
+{
+    using System;
+
+    class Weapon
+    {
+        private string _shootMode;
+        private int _bulletCount;
+        private int _reloadTimeSeconds;
+
+        public Weapon(string shootMode, int bulletCount, int reloadTimeSeconds)
+        {
+            _shootMode = shootMode;
+            _bulletCount = bulletCount;
+            _reloadTimeSeconds = reloadTimeSeconds;
+        }
+
+        public void Shoot()
+        {
+            Console.WriteLine("1 gülle atıldı.");
+            _bulletCount--;
+        }
+
+        public void Fire()
+        {
+            Console.WriteLine($"Daraq boşaldı. Bitme vaxdı: {_reloadTimeSeconds} saniye.");
+            _bulletCount = 0;
+        }
+
+        public int GetRemainBulletCount()
+        {
+            return _bulletCount;
+        }
+
+        public void Reload()
+        {
+            _bulletCount = GetRemainBulletCount();
+            Console.WriteLine("Daraq yenidən dolduruldu.");
+        }
+
+        public void ChangeFireMode()
+        {
+            if (_shootMode == "tekli")
+            {
+                _shootMode = "avtomatik";
+            }
+            else if (_shootMode == "avtomatik")
+            {
+                _shootMode = "tekli";
+            }
+            Console.WriteLine($"Atış modu deyişdirildi. Yeni mod: {_shootMode}");
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Atış modu: {_shootMode}");
+            Console.WriteLine($"Gülle sayı: {_bulletCount}");
+            Console.WriteLine($"Sıfırlama saniyesi: {_reloadTimeSeconds}");
+        }
+    }
+
+}
---
+++ WeaponTask/Program.cs
@@ -0,0 +1,60 @@
+﻿using WeaponTask.Models;
+
+
+namespace WeaponTask
+{
+    class Program
+    {
+        static void Main()
+        {
+            Weapon weapon = new Weapon("tekli", 30, 7);
+
+            while (true)
+            {
+                Console.WriteLine("0 - İnformasiya almaq üçün");
+                Console.WriteLine("1 - Shoot metodu üçün");
+                Console.WriteLine("2 - Fire metodu üçün");
+                Console.WriteLine("3 - GetRemainBulletCount metodu üçün");
+                Console.WriteLine("4 - Reload metodu üçün");
+                Console.WriteLine("5 - ChangeFireMode metodu üçün");
+                Console.WriteLine("6 - Proqramdan dayandırmaq üçün");
+                Console.WriteLine("qısayoldur");
+
+                Console.Write("Seçiminizi daxil edin: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "0":
+                        weapon.ShowInfo();
+                        break;
+                    case "1":
+                        weapon.Shoot();
+                        break;
+                    case "2":
+                        weapon.Fire();
+                        break;
+                    case "3":
+                        Console.WriteLine($"Qalan güle sayı: {weapon.GetRemainBulletCount()}");
+                        break;
+                    case "4":
+                        weapon.Reload();
+                        break;
+                    case "5":
+                        weapon.ChangeFireMode();
+                        break;
+                    case "6":
+                        Console.WriteLine("Proqram dayandırılır...");
+                        return;
+                    case "qısayoldur":
+                        Console.WriteLine("Proqram dayandırılır...");
+                        return;
+                    default:
+                        Console.WriteLine("Yanlış seçim!");
+                        break;
+                }
+
+            }
+        }
+    }
+}
